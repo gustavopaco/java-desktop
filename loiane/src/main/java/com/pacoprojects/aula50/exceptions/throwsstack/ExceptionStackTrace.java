e4ed8585f2e84554728a16261974e115bcc0ef48
Gustavo Paco <gustavopaco@gmail.com>
2023-07-23 10:09:54 -0300

    Introdução ao tratamento de erros, clausula "throws" e "printStackTrace()"

---
+++ loiane/src/main/java/com/pacoprojects/aula50/exceptions/throwsstack/ExceptionStackTrace.java
@@ -0,0 +1,17 @@
+package com.pacoprojects.aula50.exceptions.throwsstack;
+
+public class ExceptionStackTrace {
+    public static void main(String[] args) {
+        int[] numeros = {4, 8, 16, 32, 64, 128};
+        int[] denominador = {2, 0, 4, 8, 0};
+
+        for (int i = 0; i < numeros.length; i++) {
+            try {
+                System.out.println("Numeros: " + numeros[i] + " / " + denominador[i] + " = " + (numeros[i] / denominador[i]));
+            } catch (Exception exception) {
+                System.out.println(exception.getMessage());
+                exception.printStackTrace();
+            }
+        }
+    }
+}
---
+++ loiane/src/main/java/com/pacoprojects/aula50/exceptions/throwsstack/ExceptionThrows.java
@@ -0,0 +1,23 @@
+package com.pacoprojects.aula50.exceptions.throwsstack;
+
+import java.util.Scanner;
+
+public class ExceptionThrows {
+    public static void main(String[] args) {
+        System.out.println("Entre com um numero: ");
+        try {
+            double num = lerNumero();
+            System.out.println("Você digitou " + num);
+        } catch (Exception e) {
+            System.out.println("Entrada inválida");
+            e.printStackTrace();
+        }
+    }
+
+    public static double lerNumero() throws Exception{
+        Scanner scanner = new Scanner(System.in);
+        double num = scanner.nextDouble();
+        scanner.nextLine();
+        return num;
+    }
+}
