de9a8793e813bc2ea8f3f84402308584171c261c
Gustavo Paco <gustavopaco@gmail.com>
2023-07-28 23:40:42 -0300

    Introdução imports staticos

---
+++ loiane-intermediario/src/main/java/com/pacoprojects/aula59/staticimport/Boot.java
@@ -0,0 +1,19 @@
+package main.java.com.pacoprojects.aula59.staticimport;
+
+//import static java.lang.Math.*;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
+public class Boot {
+    public static void main(String[] args) {
+        double a = 2;
+        double b = 3;
+        double c = 4;
+
+        // Potencia a^b
+        System.out.println(pow(a,b));
+
+        // Raiz quadrada
+        System.out.println(sqrt(c));
+    }
+}
