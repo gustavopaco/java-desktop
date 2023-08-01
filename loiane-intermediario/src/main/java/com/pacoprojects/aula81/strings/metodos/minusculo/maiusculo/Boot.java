1dc88985a57e1ff6591161926f63dca59fee8cf7
Gustavo Paco <gustavopaco@gmail.com>
2023-08-01 14:37:44 -0300

    Introdução a metodos da String para transformacao minusculo e maiusculo

---
+++ loiane-intermediario/src/main/java/com/pacoprojects/aula81/strings/metodos/minusculo/maiusculo/Boot.java
@@ -0,0 +1,12 @@
+package main.java.com.pacoprojects.aula81.strings.metodos.minusculo.maiusculo;
+
+public class Boot {
+    public static void main(String[] args) {
+        String teste = "Teste";
+
+        // Note: toLowerCase() -> Cria um nova String com todos os caracteres em minusculo.
+        System.out.println(teste.toLowerCase());
+        // Note: toUpperCase() -> Cria um nova String com todos os caracteres em maiusculo.
+        System.out.println(teste.toUpperCase());
+    }
+}
