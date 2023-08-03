761ca9c7eba8a14e1dc078bec213163bb81e162e
Gustavo Paco <gustavopaco@gmail.com>
2023-08-03 12:23:26 -0300

    Introdução instalacao e utilizacao de LIBs Jars, "Apache Commons".
    Finalizando Curso de Java Intermediário.

---
+++ loiane-intermediario/src/main/java/com/pacoprojects/aula100/util/jars/Boot.java
@@ -0,0 +1,10 @@
+package main.java.com.pacoprojects.aula100.util.jars;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Boot {
+    public static void main(String[] args) {
+
+        System.out.println(StringUtils.capitalize("gustavo"));
+    }
+}
