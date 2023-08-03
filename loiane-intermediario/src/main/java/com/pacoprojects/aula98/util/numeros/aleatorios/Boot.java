3f9517ee4e96bdf18f0121568563ab1e9c6e967d
Gustavo Paco <gustavopaco@gmail.com>
2023-08-03 10:56:33 -0300

    Introdução as Classes Utilitarias. Para gerar Numeros Aleatorios, "Random".

---
+++ loiane-intermediario/src/main/java/com/pacoprojects/aula98/util/numeros/aleatorios/Boot.java
@@ -0,0 +1,17 @@
+package main.java.com.pacoprojects.aula98.util.numeros.aleatorios;
+
+import java.util.Random;
+
+public class Boot {
+    public static void main(String[] args) {
+
+        System.out.println(Math.floor(Math.random() * 10));
+
+        Random random = new Random();
+
+        // Note: nextInt() -> Gera numero aleatorio do 0 ate o tamanho do INT
+        System.out.println(random.nextInt());
+        // Note: nextInt(VALOR) -> Gera numero aleatorio de 0 a 10
+        System.out.println(random.nextInt(11));
+    }
+}
