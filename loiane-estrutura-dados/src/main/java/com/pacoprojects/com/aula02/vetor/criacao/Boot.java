f05ec22d3d7669cfc07430e453f20402a5b2a043
Gustavo Paco <gustavopaco@gmail.com>
2023-08-04 08:46:52 -0300

    Introdução a Vetores, "Criacao".
    Iniciando Curso de Estrutura de dados e Algoritmos.

---
+++ loiane-estrutura-dados/src/main/java/com/pacoprojects/com/aula02/vetor/criacao/Boot.java
@@ -0,0 +1,12 @@
+package main.java.com.pacoprojects.com.aula02.vetor.criacao;
+
+import main.java.com.pacoprojects.com.aula02.vetor.criacao.model.Vetor;
+
+public class Boot {
+    public static void main(String[] args) {
+
+        Vetor vetor = new Vetor(5);
+
+        System.out.println("Vetor criado!");
+    }
+}
---
+++ loiane-estrutura-dados/src/main/java/com/pacoprojects/com/aula02/vetor/criacao/model/Vetor.java
@@ -0,0 +1,9 @@
+package main.java.com.pacoprojects.com.aula02.vetor.criacao.model;
+
+public class Vetor {
+    private String[] elementos;
+
+    public Vetor(int capacidade) {
+        elementos = new String[capacidade];
+    }
+}
